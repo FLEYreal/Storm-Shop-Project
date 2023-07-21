11cc15651acb3b1488e1ce18b0fc7eb97784e822
FLEYreal <borisov.nikita.off@gmail.com>
2023-07-22 02:21:43 +0800

    some changes

--- frontend/src/utils/lighterRGB.ts
+++ frontend/src/utils/lighterRGB.ts
@@ -1,18 +1,18 @@
-export default function lighterRgb(rgba:string, color:number | string) {
-    // Получить уровень прозрачности изначального цвета
-    let value = rgba
-      .split(/[()]/)[1]
-      .split(',')[3];
-  
-    // Найти все совпадения
-    let matches = rgba.match(new RegExp(value, 'g'));
-  
-    // Найти индекс последнего совпадения
-    let lastIndex = rgba.lastIndexOf(matches![matches!.length - 1]);
-  
-    // Разделить строку на две части и заменить значение
-    let modifiedString = rgba.substring(0, lastIndex) + ' ' + String(color) + rgba.substring(lastIndex + value.length);
-  
-    return modifiedString;
-  }
-  
+// Данная функция делает принимаемый цвет той прозрачности, которую мы передали вторым аргументом
+export default function lighterRgb(rgba: string, color: number | string) {
+  // Получить уровень прозрачности изначального цвета
+  let value = rgba
+    .split(/[()]/)[1]
+    .split(',')[3];
+
+  // Найти все совпадения
+  let matches = rgba.match(new RegExp(value, 'g'));
+
+  // Найти индекс последнего совпадения
+  let lastIndex = rgba.lastIndexOf(matches![matches!.length - 1]);
+
+  // Разделить строку на две части и заменить значение
+  let modifiedString = rgba.substring(0, lastIndex) + ' ' + String(color) + rgba.substring(lastIndex + value.length);
+
+  return modifiedString;
+}
