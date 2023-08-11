07b1cbdef805c92f1874cce50d28b2cf12f310bc
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-11 23:55:40 +0800

    Footer is completed

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,14 +3,48 @@ import useResolutions from '../../hooks/useResolusions';
 import resStyles from '../../utils/resStyles';
 import styles from './styles.module.scss'
 
+// Картинки
+import logo from '../img/logo512.png'
+
 function Footer() {
 
     const resolution = useResolutions();
+    const { isSmallScreen, isPhone } = useResolutions();
 
-    return ( 
+    return (
         <footer className={`${styles.footer}`}>
             <div className={`${resStyles('container', resolution)}`}>
-                
+                <div className={`${styles.top}`}>
+                    {!isPhone ?
+                        <div className={`${styles.column} ${styles.logo_column}`}>
+                            <img src={logo} alt='StormShop Logo' className={`${resStyles('footer_logo', resolution)}`} />
+                        </div>
+                        : <></>
+                    }
+                    <div className={`${styles.column}`}>
+                        <div className={`${styles.title} ${resStyles('smallTitle', resolution)}`}>Заголовок</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                    </div>
+                    <div className={`${styles.column}`}>
+                        <div className={`${styles.title} ${resStyles('smallTitle', resolution)}`}>Заголовок</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                        <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                    </div>
+                    {isSmallScreen && isPhone ? <></> :
+                        <div className={`${styles.column}`}>
+                            <div className={`${styles.title} ${resStyles('smallTitle', resolution)}`}>Заголовок</div>
+                            <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                            <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                            <div className={`${styles.subtitle} ${resStyles('smallSubtitle', resolution)}`}>Пункт</div>
+                        </div>
+                    }
+                </div>
+                <div className={`${styles.bottom}`}>
+                    © {new Date().getFullYear()} StormShop
+                </div>
             </div>
         </footer>
     );
