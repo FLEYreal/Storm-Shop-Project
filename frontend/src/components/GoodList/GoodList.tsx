439977096c4c403c7f729692911df6ca2997b776
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-17 21:28:16 +0300

    GoodList component

---
+++ frontend/src/components/GoodList/GoodList.tsx
@@ -0,0 +1,66 @@
+// Базовые импорты
+import { useContext, useEffect, useState } from 'react'
+import { AxiosResponse } from 'axios';
+
+// Стили
+import styles from './styles.module.scss';
+import resStyles from '../../utils/resStyles.js';
+import useResolutions from '../../hooks/useResolusions';
+
+// Компоненты & Хуки проекта
+import Good from '../Good/Good'
+
+// API импорты
+import { APIContext } from '../../context/APIContext'
+
+// Интерфейс для товара
+import { GoodType } from '../../typings/Good'
+
+export default function GoodList({ type = 'sub' }: { type?: string }) {
+    // Список товаров
+    const [goodList, setGoodlist] = useState<GoodType[]>([]);
+
+    // Получить объект с разрешением экрана
+    const resolutions = useResolutions()
+
+    // Класс для работы с API
+    const api = useContext(APIContext)!.api
+
+    // Получить список товаров
+    useEffect(() => {
+        (async () => {
+            if (type === 'sub') {
+                const res = await api?.getSubList() as { data: AxiosResponse["data"] };
+                setGoodlist(res!.data)
+            } else if (type === 'script') {
+                const res = await api?.getScriptList() as { data: AxiosResponse["data"] };
+                setGoodlist(res!.data)
+            }
+        })()
+    }, [])
+
+    return (
+        // Тэг для хранения списка товаров
+        <section className={`${styles.container} ${resStyles('goodList', resolutions)} ${resStyles('container', resolutions)}`}>
+
+            {/* Вывод списка товаров */}
+            {
+                goodList.length > 0 ? goodList.map(good =>
+                    <Good
+                        key={good.id}
+                        desc={{
+                            title: good.displayName,
+                            subtitle: good.desc,
+                            cost: good.cost,
+                            image: good.imageUrl,
+                            theme: good.themeColor,
+                            rating: good.rating,
+                            reviews: good.reviews,
+                            old_cost: good.old_cost
+                        }}
+                    />
+                ) : null
+            }
+        </section>
+    )
+}
--- frontend/src/components/blocks/App/Block_3/Block_3.tsx
+++ frontend/src/components/blocks/App/Block_3/Block_3.tsx
@@ -1,38 +1,14 @@
-// Базовые импорты
-import { useContext, useEffect, useState } from 'react'
-import { AxiosResponse } from 'axios';
-
 // Стили
 import styles from './styles.module.scss';
 import resStyles from '../../../../utils/resStyles.js';
 import useResolutions from '../../../../hooks/useResolusions';
 
 // Компоненты & Хуки проекта
-import Good from '../../../Good/Good'
-
-// API импорты
-import { APIContext } from '../../../../context/APIContext'
-
-// Интерфейс для товара
-import { GoodType } from '../../../../typings/Good'
+import GoodList from '../../../GoodList/GoodList.js';
 
 export default function Block_3() {
-    // Список товаров
-    const [goodList, setGoodlist] = useState<GoodType[]>([]);
-
     // Получить объект с разрешением экрана
     const resolutions = useResolutions()
-
-    // Класс для работы с API
-    const api = useContext(APIContext)!.api
-
-    // Получить список товаров
-    useEffect(() => {
-        (async () => {
-            const res = await api?.getSubList() as { data: AxiosResponse["data"] };
-            setGoodlist(res!.data)
-        })()
-    }, [])
 
     return (
         <>
@@ -48,27 +24,7 @@ export default function Block_3() {
                 </div>
 
                 {/* Тэг для хранения списка товаров */}
-                <div className={`${styles.container} ${resStyles('goodList', resolutions)} ${resStyles('container', resolutions)}`}>
-
-                    {/* Вывод списка товаров */}
-                    {
-                        goodList.length > 0 ? goodList.map(good =>
-                            <Good
-                                key={good.id}
-                                desc={{
-                                    title: good.displayName,
-                                    subtitle: good.desc,
-                                    cost: good.cost,
-                                    image: good.imageUrl,
-                                    theme: good.themeColor,
-                                    rating: good.rating,
-                                    reviews: good.reviews,
-                                    old_cost: good.old_cost
-                                }}
-                            />
-                        ) : null
-                    }
-                </div>
+                <GoodList type='sub'/>
             </section>
         </>
     )
