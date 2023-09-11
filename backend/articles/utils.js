27fd8f4b3f5ca673117956dd10ace9a85e801948
FLEYreal <borisov.nikita.off@gmail.com>
2023-09-11 20:10:00 +0800

    delete backend files from frontend branch

--- backend/articles/utils.js
+++
@@ -1,9 +0,0 @@
-const articles = require('./articles.json')
-
-function findArticle(uniqueName) {
-    return articles.find(i => i.unique_name === uniqueName)
-}
-
-module.exports = {
-    findArticle
-}
--- telegram_bot/keyboards/__init__.py
+++
@@ -1,2 +0,0 @@
-from keyboards.client_kb import catalog_list, earnings_list, buy_list, keyboard_bot_tg_and_ds, by_keyboard
-
